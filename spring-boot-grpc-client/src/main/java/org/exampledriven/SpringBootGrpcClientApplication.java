5026c1e0c46db8ffead31d7f2dbc6c480846561d
Peter Szanto <git@szantocsalad.hu>
2016-10-18 13:20:58 +0200

    no not register the client app as a service

--- spring-boot-grpc-client/src/main/java/org/exampledriven/SpringBootGrpcClientApplication.java
+++ spring-boot-grpc-client/src/main/java/org/exampledriven/SpringBootGrpcClientApplication.java
@@ -2,10 +2,11 @@ package org.exampledriven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class SpringBootGrpcClientApplication {
 
     public static void main(String[] args) {
